7f09d3d8fdbee4bf410923e307d9f2d7f5f382c1
matialvarez7 <matias.gabriel.alvarez@gmail.com>
2022-11-22 22:17:37 -0300

    seed de device

--- db/seeders/20221122233619-demo-device.js
+++ db/seeders/20221122233619-demo-device.js
@@ -29,6 +29,6 @@ module.exports = {
 
   async down (queryInterface, Sequelize) {
 
-    await queryInterface.bulkDelete('People', null, {});
+    await queryInterface.bulkDelete('Devices', null, {});
   }
 };
