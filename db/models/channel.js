936a01453128acb865c147f8c3bba8757241256a
matialvarez7 <matias.gabriel.alvarez@gmail.com>
2022-12-01 20:37:14 -0300

    añadido el modelo de channels

---
+++ db/models/channel.js
@@ -0,0 +1,35 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Channel extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Channel.init({
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    logo_url: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
+  }, {
+    sequelize,
+    modelName: 'Channel',
+    tableName: 'Channels'
+  });
+  return Channel;
+};
