a37bfa5154e0c6b0cd2953e07d976bb003bf315f
matialvarez7 <matias.gabriel.alvarez@gmail.com>
2022-12-01 20:55:22 -0300

    metodos para buscar todos los canales y un canal en particular

--- routes/channels.js
+++ routes/channels.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+const Channel = require('../src/repositories/channels');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async function(req, res, next) {
+  res.status(200).json(await Channel.allChannels());
+});
+
+router.get('/:id', async function(req, res, next) {
+    res.status(200).json(await Channel.findChannel(req.params.id));
 });
 
 module.exports = router;
