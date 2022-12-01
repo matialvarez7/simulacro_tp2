5e1b4374e10960f32718dca9732ed206df5b92c8
matialvarez7 <matias.gabriel.alvarez@gmail.com>
2022-12-01 20:56:23 -0300

    seed para channels

---
+++ db/seeders/20221201234357-channels.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+
+    await queryInterface.bulkInsert('Channels', [{
+      name: 'America Tv',
+      logo_url: 'america.jpg',
+      category_id: 1,
+      createdAt: "2022-11-22 20:45:00",
+      updatedAt: "2022-11-22 20:45:00"
+    },
+    {
+      name: 'Telefe',
+      logo_url: 'telefe.jpg',
+      category_id: 1,
+      createdAt: "2022-11-22 20:45:00",
+      updatedAt: "2022-11-22 20:45:00"
+    },{
+      name: 'TyC Sports',
+      logo_url: 'tyc.jpg',
+      category_id: 2,
+      createdAt: "2022-11-22 20:45:00",
+      updatedAt: "2022-11-22 20:45:00"
+    }
+  ], {});
+  },
+
+  async down (queryInterface, Sequelize) {
+
+    await queryInterface.bulkDelete('Channels', null, {});
+
+  }
+};
