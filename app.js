09692b9d0cf926c32e0e872b03b561ac523a91d9
matialvarez7 <matias.gabriel.alvarez@gmail.com>
2022-12-01 20:38:28 -0300

    agregada la ruta de channels

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var devicesRouter = require('./routes/devices');
 var categoriesRouter = require('./routes/categories');
+var channelsRouter = require('./routes/channels');
 
 var app = express();
 
@@ -20,5 +21,6 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/devices', devicesRouter);
 app.use('/categories', categoriesRouter);
+app.use('/channels', channelsRouter);
 
 module.exports = app;
