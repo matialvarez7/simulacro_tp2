1ceff2b61454dadf8940b459a359d6533cfbf345
matialvarez7 <matias.gabriel.alvarez@gmail.com>
2022-12-05 16:40:45 -0300

    testing devices

--- test/devices.js
+++ test/devices.js
@@ -20,7 +20,7 @@ describe('Autenticacion de dispositivo', function(){
     })
 
     describe('Eliminacion de un dispositivo', function(){
-        it('Si el dispositivo existe puedo eliminarlo', function(done){
+        it.skip('Si el dispositivo existe puedo eliminarlo', function(done){
             request(app)
                 .delete('/devices/M1A9T9I1A0S1')
                 .expect(200)
@@ -31,4 +31,60 @@ describe('Autenticacion de dispositivo', function(){
                 })
         })
     })
-})
+});
+
+describe('Canales favoritos', function(){
+    describe('Adheris canales a favoritos de un sipositivo', function(){
+        it.skip('Si el dispositvo no existe no puedo adherir a favoritos', function(done){
+            request(app)
+                .post('/devices/5/favorites')
+                .send({
+                    channelId: 1
+                })
+                .expect(404)
+                .end(function(err, res){
+                    assert.equal(res.body.message, "DEVICE_NOT_FOUND")
+                    if(err) done(err)
+                    return done()
+                })
+
+        })
+
+        it.skip('Si el dispositivo no tiene el canal no puedo adherir a favoritos', function(done){
+            request(app)
+                .post('/devices/1/favorites')
+                .send({channelId: 4})
+                .expect(400)
+                .end(function(err, res){
+                    assert.equal(res.body.message, "DEVICE_DOES_NOT_HAVE_THE_CHANNEL")
+                    if(err) done(err)
+                    return done()
+                })
+        })
+
+        it('Si el disposito no lo tiene en favoritos puedo agregarlo', function(done){
+            request(app)
+                .post('/devices/1/favorites')
+                .send({channelId: 2})
+                .expect(201)
+                .end(function(err, res){
+                    assert.equal(res.body.message, 'CHANNEL_ADDED_SUCCESSFULLY')
+                    if(err) done(err)
+                    return done()
+                })
+
+        })
+
+        it.skip('Si el dispositivo ya lo tiene en favoritos no puedo agregarlo', function(done){
+            request(app)
+                .post('/devices/1/favorites')
+                .send({channelId: 3})
+                .expect(400)
+                .end(function(err, res){
+                    assert.equal(res.body.message, "CHANNEL_ALREADY_EXIST_IN_FAVORITES")
+                    if(err) done(err)
+                    return done()
+                })
+        })
+    })
+});
