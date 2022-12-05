7e07f1044f49d38bb6821c40ce5f7c8a5b5d9a80
matialvarez7 <matias.gabriel.alvarez@gmail.com>
2022-12-05 16:35:56 -0300

    agregada la relación con channel y favoritesw

--- db/models/device.js
+++ db/models/device.js
@@ -10,7 +10,8 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Device.hasMany(models.Channel, {foreignKey: 'deviceId'})
+      Device.hasMany(models.Favorite, {foreignKey: 'deviceId'})
     }
   }
   Device.init({
