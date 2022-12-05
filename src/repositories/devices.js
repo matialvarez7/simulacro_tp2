0042d4a76e1cc796f39935ac26a3d1bb416fbb4f
matialvarez7 <matias.gabriel.alvarez@gmail.com>
2022-12-05 16:40:14 -0300

    metodos para agregar a canales favoritos

--- src/repositories/devices.js
+++ src/repositories/devices.js
@@ -1,4 +1,4 @@
-const {Device} = require('../../db/models');
+const {Device, Channel, Favorite} = require('../../db/models');
 
 async function identifierExist(identifier){
     return await Device.findOne({
@@ -8,6 +8,30 @@ async function identifierExist(identifier){
     }) 
 }
 
+async function hasTheChannel(deviceId, channelId){
+    return await Device.findOne({
+        where: {id: deviceId},
+        attributes: [],
+        include:{
+            model: Channel,
+            where:{id: channelId}
+        }
+    })
+}
+
+async function existInFavorites(deviceId, channelId){
+    return await Device.findOne({
+        where:{id: deviceId},
+        include:{
+            model:Favorite,
+            where:{cId: channelId}
+        }
+    })
+}
+
+async function idByPkExist(id){
+    return await Device.findByPk(id);
+}
 async function newDevice(identifier){
     return await Device.create({
         identifier
@@ -23,8 +47,42 @@ async function deleteDevice(identifier){
     }
 }
 
+async function addChannelToFavorites(deviceId, channelId){
+    let myDevice = await idByPkExist(deviceId);
+    let channelObject = await hasTheChannel(deviceId, channelId);
+    let myArray = channelObject.Channels;
+    let myChannel = myArray[0];
+    await myDevice.createFavorite({
+        cId: myChannel.id,
+        name: myChannel.name,
+        logo_url: myChannel.logo_url,
+        category_id: myChannel.category_id,
+        deviceId
+    })
+}
+
+async function getAllChannels(id){
+    return await Device.findOne({
+        where: {id},
+        include: Channel
+    })
+}
+
+async function getAllFavorites(id){
+    return await Device.findOne({
+        where:{id},
+        include: Favorite
+    })
+}
+
 module.exports = {
     identifierExist,
     newDevice,
-    deleteDevice
+    deleteDevice,
+    idByPkExist,
+    hasTheChannel,
+    existInFavorites,
+    addChannelToFavorites,
+    getAllChannels,
+    getAllFavorites
 }
